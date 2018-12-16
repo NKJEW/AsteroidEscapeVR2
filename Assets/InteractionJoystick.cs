0117b91a5b6846b68877c88000eab42e73a4c350
Jack Southard <jacksouthard265@gmail.com>
2018-12-16 09:33:10 -0800

    Add basic interaction sustem

---
+++ Assets/InteractionJoystick.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionJoystick : InteractionObject {
+    public Transform rotBase;
+    public float maxAngle;
+
+    void Update () {
+        if (interacting) {
+            Vector3 targetEulers = interactingCon.transform.localEulerAngles;
+            Vector3 clampedEulers = new Vector3(Mathf.Clamp(targetEulers.x, -maxAngle, maxAngle), 0f, Mathf.Clamp(targetEulers.z, -maxAngle, maxAngle));
+            rotBase.rotation = Quaternion.Euler(clampedEulers);
+
+            // calculate output
+            float x = rotBase.localEulerAngles.x / maxAngle;
+            float z = rotBase.localEulerAngles.z / maxAngle;
+            output = new Vector2(x, z);
+        }
+	}
+}
