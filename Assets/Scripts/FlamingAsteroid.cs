53bc783a211140f26c93783da442b9749bc17c0a
Nathaniel Jew <nathankjew@gmail.com>
2019-01-19 21:20:35 -0800

    Trails to flaming asteroids

---
+++ Assets/Scripts/FlamingAsteroid.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlamingAsteroid : MonoBehaviour {
+    public GameObject explosionPart;
+    public Material redMat;
+    public Material smokeMat;
+
+    float nextSpawn;
+
+    void LateUpdate() {
+        if (Time.time > nextSpawn) {
+            nextSpawn = Time.time + 0.05f;
+
+            //GameObject redPart = CreateNewPart(0f);
+            //redPart.GetComponent<Renderer>().material = redMat;
+
+            GameObject smokePart = CreateNewPart(0);
+            smokePart.GetComponent<Renderer>().material = smokeMat;
+        }
+    }
+
+    GameObject CreateNewPart(float delay) {
+        Vector3 offset = Random.insideUnitSphere * 0.3f;
+        GameObject newPart = Instantiate(explosionPart, transform.position + offset + (transform.right * 3), Random.rotation);
+        newPart.GetComponent<ExplosionParticle>().Init(delay, 1f, Random.Range(1f, 1.25f), offset);
+        return newPart;
+    }
+}
